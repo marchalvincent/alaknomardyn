95a66936e651a6d34538aad0167bd7389703f3c0
marchal vin <marchal.vin@gmail.com>
2013-09-25 14:54:37 +0200

    push temp

--- 3150235-2900107/src/fr/upmc/ta/aladyn/backup/BackupManager.java
+++ 3150235-2900107/src/fr/upmc/ta/aladyn/backup/BackupManager.java
@@ -2,8 +2,9 @@ package fr.upmc.ta.aladyn.backup;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-
-import fr.upmc.ta.aladyn.tests.MyPoint;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Cette classe permet d'enregistrer l'état d'un objet puis de le restorer.
@@ -13,8 +14,13 @@ import fr.upmc.ta.aladyn.tests.MyPoint;
  * @param <T> le type de la classe à enregistrer.
  */
 public class BackupManager<T> {
-
-	private T backup;
+	
+	private Map<Field, Object> values;
+	
+	public BackupManager() {
+		super();
+		values = new HashMap<>();
+	}
 
 	/***
 	 * Méthode permettant de sauvegarder un objet à un instant T
@@ -25,33 +31,26 @@ public class BackupManager<T> {
 	 * @throws InstantiationException 
 	 * @throws Exception
 	 */
-	public void backup(T objectToSave) throws Exception {
+	public void save(T objectToSave) throws Exception {
 
-		// on récupère la classe de l'objet, ainsi que ses fields
-		Class<?> classObject = objectToSave.getClass();
-		Field[] fields = classObject.getDeclaredFields();
-
-		// Instanciacion de l'objet backup
-		/*** ATTENTION LA CLASSE DOIT POSSEDER UN CONSTRUCTEUR SANS PARAMETRES ***/
-		// TODO gérer...
-		backup = (T) classObject.newInstance();
+		// on récupère les Field de l'objet
+		Field[] fields = objectToSave.getClass().getDeclaredFields();
 
 		// parcours de tous les attributs de l'objet afin de récupérer les valeurs des attributs et les sauvegarder
 		for (Field field : fields) {
 			field.setAccessible(true);
 
-			// on ne peut pas "set" une variable final
+			// on ne peut pas "set" une variable final, il ne sert a rien de l'avoir en mémoire
 			if (!Modifier.isFinal(field.getModifiers())) {
-				field.set(backup, field.get(objectToSave));
+				values.put(field, field.get(objectToSave));
 			}
 		}
-
-		System.out.println("SAVE");
-
-		System.out.print("tab0 ");
-		System.out.println(((MyPoint) backup).getTab0());
-		System.out.print("tab1 ");
-		System.out.println(((MyPoint) backup).getTab1());
+		
+		System.out.println("after save");
+		Set<Field> set = values.keySet();
+		for (Field field : set) {
+			System.out.println(field + " | " + values.get(field));
+		}
 	}
 
 	/***
@@ -63,34 +62,18 @@ public class BackupManager<T> {
 	 */
 	public void restore(T objectToRestore) throws Exception {
 
-		Class<?> c = objectToRestore.getClass();
+		// on récupère les Field de l'objet
+		Field[] fields = objectToRestore.getClass().getDeclaredFields();
 
-		// Récupération de l'ensembe des attributs de l'objet
-		// sous le type de Field
-		Field[] fields = c.getDeclaredFields();
-
-		// parcours de tous les attributs de backup
-		// pour récupérer les valeurs et les restaurer dans l'objet
+		// parcours de tous les attributs de backup pour récupérer les valeurs et les restaurer dans l'objet
 		for (Field field : fields) {
 			field.setAccessible(true);
 
 			// on ne peut pas "set" une variable final
 			if (!Modifier.isFinal(field.getModifiers())) {
-				field.set(objectToRestore, field.get(backup));
+				field.set(objectToRestore, values.get(field));
 			}
 		}
-
-		System.out.println("RESTORE");
-
-		System.out.print("tab0 ");
-		System.out.println(((MyPoint) backup).getTab0());
-		System.out.print("tab1 ");
-		System.out.println(((MyPoint) backup).getTab1());
-		
-	}
-
-	public T getBackup() {
-		return backup;
 	}
 
 	/***
