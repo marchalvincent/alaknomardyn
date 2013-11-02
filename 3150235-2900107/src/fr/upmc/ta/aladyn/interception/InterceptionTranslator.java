5abfb11230df0b2c63c87fa74a6ec49d72d65943
marchal vin <marchal.vin@gmail.com>
2013-11-02 22:56:57 +0100

    commentaire de syso

--- 3150235-2900107/src/fr/upmc/ta/aladyn/interception/InterceptionTranslator.java
+++ 3150235-2900107/src/fr/upmc/ta/aladyn/interception/InterceptionTranslator.java
@@ -24,7 +24,7 @@ public class InterceptionTranslator implements Translator {
 
     @Override
     public void onLoad(ClassPool pool, String classname) throws NotFoundException, CannotCompileException {
-	System.out.println("chargement de " + classname);
+//	System.out.println("chargement de " + classname);
 	CtClass ctClass = pool.get(classname);
 
 	// On regarde si la CtClass est transactionnable
